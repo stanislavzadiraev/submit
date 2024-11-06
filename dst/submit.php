ce11939bc21b13dbf8334640c0fb9e62c5f1ed52
stanislavzadiraev <zadiraeff@yandex.ru>
2024-11-06 19:33:53 +0300

    first step
    Signed-off-by: stanislavzadiraev <zadiraeff@yandex.ru>

---
+++ dst/submit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name: Submit
+ * Text Domain: submit
+ */
+
+add_shortcode(
+    'submit',
+    function ($atts) {
+        return 'THE SUBMIT';
+    }
+);
+ 
+?>
---
+++ wops.config.js
@@ -0,0 +1,10 @@
+export default {
+    dbpath: 'db',
+    wppath: 'wp',
+    dbport: '9595',
+    wpport: '8585',
+    
+    wpcontent: [
+        ['dst', 'plugins/submit'],
+      ],
+}
