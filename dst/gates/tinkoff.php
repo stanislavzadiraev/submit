f3e60e62fa6b647c2e2ea6d27fcbd15bfa17841d
stanislavzadiraev <zadiraeff@yandex.ru>
2024-11-28 13:28:39 +0300

    some shit
    Signed-off-by: stanislavzadiraev <zadiraeff@yandex.ru>

--- dst/gates/tinkoff.php
+++ dst/gates/tinkoff.php
@@ -9,14 +9,17 @@ return function($form){
             '<input type="hidden" name="terminalkey" value="1698927993527">'.
             '<input type="hidden" name="frame" value="false">'.
             '<input type="hidden" name="language" value="ru">'.
+            
+            '<input type="hidden" name="receipt" value="">'.
 
-            '<button type="submit" name="submit" class="button" text="Оплатить">Оплатить</button>'.
+            '<button type="submit" name="submit">Оплатить</button>'.
         '</form>'.
         '<script>
             document
                 .querySelector("div#pay-tinkoff>form")
                 .addEventListener("submit", e => (
-                    e.preventDefault(e)
+                    e.preventDefault(e),
+                    pay(document.querySelector("div#pay-tinkoff>form"))
                 ))
         </script>'.
         '</div>';
