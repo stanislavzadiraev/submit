fc0e83380980df447455279ea74182428dfc36fc
stanislavzadiraev <zadiraeff@yandex.ru>
2024-11-28 15:37:13 +0300

    some shit
    Signed-off-by: stanislavzadiraev <zadiraeff@yandex.ru>

--- dst/gates/tinkoff.php
+++ dst/gates/tinkoff.php
@@ -10,15 +10,37 @@ return function($form){
             '<input type="hidden" name="frame" value="false">'.
             '<input type="hidden" name="language" value="ru">'.
             
-            '<input type="hidden" name="receipt" value="">'.
+            '<input id="receipt" type="hidden" name="receipt" value="">'.
+            '<input id="amount" type="hidden" name="amount" value="">'.
 
             '<button type="submit" name="submit">Оплатить</button>'.
         '</form>'.
         '<script>
+            const label = "item"
+            const value = "price"
             document
                 .querySelector("div#pay-tinkoff>form")
                 .addEventListener("submit", e => (
                     e.preventDefault(e),
+                    document
+                        .querySelector("div#pay-tinkoff>form")
+                        .receipt.value = JSON.stringify({
+                            "EmailCompany": "mail@mail.com",
+                            "Taxation": "patent",
+                            "FfdVersion": "1.2",
+                            "Items": [
+                                {
+                                    "Name": label,
+                                    "Price": value * 100,
+                                    "Quantity": 1.00,
+                                    "Amount": value * 100,
+                                    "PaymentMethod": "full_prepayment",
+                                    "PaymentObject": "service",
+                                    "Tax": "none",
+                                    "MeasurementUnit": "pc"
+                                }
+                            ]
+                        }),
                     pay(document.querySelector("div#pay-tinkoff>form"))
                 ))
         </script>'.
