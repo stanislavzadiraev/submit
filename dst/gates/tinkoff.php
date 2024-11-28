5f673ef1a0ee0df1996e111447e24ff6e4e950c7
stanislavzadiraev <zadiraeff@yandex.ru>
2024-11-28 12:56:37 +0300

    some shit
    Signed-off-by: stanislavzadiraev <zadiraeff@yandex.ru>

--- dst/gates/tinkoff.php
+++ dst/gates/tinkoff.php
@@ -4,7 +4,21 @@ return function($form){
     return 
         $form.
         '<div id="pay-tinkoff">'.
-            'SOME SHIT'.
+        '<script src="https://securepay.tinkoff.ru/html/payForm/js/tinkoff_v2.js"></script>'.
+        '<form id="payform-tbank" name="pay-tinkoff" >'.
+            '<input type="hidden" name="terminalkey" value="1698927993527">'.
+            '<input type="hidden" name="frame" value="false">'.
+            '<input type="hidden" name="language" value="ru">'.
+
+            '<button type="submit" name="submit" class="button" text="Оплатить">Оплатить</button>'.
+        '</form>'.
+        '<script>
+            document
+                .getElementById("payform-tbank")
+                .addEventListener("submit", e => (
+                    preventDefault(e)
+                ))
+        </script>'.
         '</div>';
 }
 
